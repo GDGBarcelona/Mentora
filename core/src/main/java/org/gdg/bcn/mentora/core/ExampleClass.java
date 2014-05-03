a47a878092a4d90a7293459c857fd9ada0596676
Akelael <akelael@gmail.com>
2014-05-03 13:24:59 +0200

    Sesion 3

--- core/src/main/java/org/gdg/bcn/mentora/core/ExampleClass.java
+++ core/src/main/java/org/gdg/bcn/mentora/core/ExampleClass.java
@@ -1,4 +1,11 @@
 package org.gdg.bcn.mentora.core;
 
 public class ExampleClass {
+    public String makeHash(String s) throws makeHashNullString {
+        if (s==null) throw new makeHashNullString();
+        if (s.equals("a")) return "ASDF";
+        return "";
+    }
+
+    public class makeHashNullString extends Exception {}
 }
